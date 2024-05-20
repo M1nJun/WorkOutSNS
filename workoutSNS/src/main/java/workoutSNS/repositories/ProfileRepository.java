d2efe429abf7606047f9e63ec3c2b0a01c15a1cd
Mohamed-Khalil-Ben-Nasr <mohamedkhalil.bennasr@lawrence.edu>
2024-05-20 13:43:11 -0500

    need to work on back end / encountered issues in the build path and dependencies

--- workoutSNS/src/main/java/workoutSNS/repositories/ProfileRepository.java
+++ workoutSNS/src/main/java/workoutSNS/repositories/ProfileRepository.java
@@ -3,6 +3,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import workoutSNS.entities.Profile;
 
+
 public interface ProfileRepository extends JpaRepository<Profile,Integer> {
 
 }
