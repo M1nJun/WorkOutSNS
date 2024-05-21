fccfe06791c3e46fbe22d3e4e17ca1276a41686a
Minjun Lee <lmjg0407@gmail.com>
2024-05-21 14:57:49 -0500

    backend without error and efforts to have it connected to the front. Backend works fine on postman.

--- WorkoutSNS-Front-End/src/components/HomePage.jsx
+++ WorkoutSNS-Front-End/src/components/HomePage.jsx
@@ -1,54 +1,78 @@
-import { useRef, useState } from 'react';
-import { processJSON, processText } from '../FetchRoutines';
-import { useContext } from 'react';
-import AuthContext from '../AuthContext';
-import { Button, TextField,Box, Container, Typography, Grid,InputAdornment, IconButton } from '@mui/material';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { useRef, useState } from "react";
+import { processJSON, processText } from "../FetchRoutines";
+import { useContext } from "react";
+import AuthContext from "../AuthContext";
+import {
+  Button,
+  TextField,
+  Box,
+  Container,
+  Typography,
+  Grid,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import logo from "../logo.png";
 
-function HomePage({setJwt}) {
+function HomePage({ setJwt }) {
   const jwt = useContext(AuthContext);
-  const [showPassword,setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
+  function confirmLogin(jwt) {
+    alert("You are logged in to your account.");
+    setJwt(jwt);
+  }
 
-    function confirmLogin(jwt) {
-      alert("You are logged in to your account.");
-      setJwt(jwt);
-    }
+  function handleLogin(user) {
+    fetch("http://localhost:8085/users/login", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then(processJSON)
+      .then(confirmLogin)
+      .catch(() => {
+        alert("Login failed");
+      });
+  }
 
-    function handleLogin(user) {
-        fetch('http://localhost:8085/user/login', {
-          method: 'POST',
-          body: JSON.stringify(user),
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-          }
-        })
-        .then(processJSON)
-        .then(confirmLogin)
-        .catch(() => { alert("Login failed"); });
-    }
-
-    function handleNewAccount(user) {
-      fetch('http://localhost:8085/user/create', {
-          method: 'POST',
-          body: JSON.stringify(user),
-          headers: {
-          'Content-type': 'application/json; charset=UTF-8'
-          }
-      })
+  function handleNewAccount(user) {
+    fetch("http://localhost:8085/users", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
       .then(processText)
       .then(confirmLogin)
-      .catch(() => {alert("Create new account failed")});
-    }
+      .catch(() => {
+        alert("Create new account failed");
+      });
+  }
 
-    let nameInput = useRef(null);
-    let passwordInput = useRef(null);
+  let nameInput = useRef(null);
+  let passwordInput = useRef(null);
 
-    const loginAction = (e) => { handleLogin({name:nameInput.current.value,password:passwordInput.current.value}); }
-    const newAction = (e) => { handleNewAccount({name:nameInput.current.value,password:passwordInput.current.value}); }
-    const handleTogglePasswordVisibility = () => {setShowPassword(!showPassword);};
-    
+  const loginAction = (e) => {
+    handleLogin({
+      username: nameInput.current.value,
+      password: passwordInput.current.value,
+    });
+  };
+  const newAction = (e) => {
+    handleNewAccount({
+      username: nameInput.current.value,
+      password: passwordInput.current.value,
+    });
+  };
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div>
       {jwt ? (
@@ -58,86 +82,96 @@ function HomePage({setJwt}) {
             {/* The option to log out should be under account tab in the navbar */}
             <h4>Log out of your account</h4>
             <p>
-              <Button variant="contained" color="primary" onClick={() => setJwt(null)}>Log Out</Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => setJwt(null)}
+              >
+                Log Out
+              </Button>
             </p>
           </div>
         </Container>
-        ):(
-    <Container maxWidth="sm">
-      <Box my={1}>
-        <Box display="flex" justifyContent="center" mb={2}>
-          <img src={logo} alt="Logo" style={{ maxWidth: '100%', height: 'auto' }} />
-        </Box>
-        <Typography variant="h4" align="center" gutterBottom>
-          Welcome to
-        </Typography>
-        <Typography variant="h2" align="center" gutterBottom>
-          Fit Connect
-        </Typography>
-        <Typography variant="h4" align="center" gutterBottom>
-          Log in to your account
-        </Typography>
-        <Box component="form" noValidate autoComplete="off">
-          <Grid container spacing={2} justifyContent="center">
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Username"
-                variant="outlined"
-                inputRef={nameInput}
+      ) : (
+        <Container maxWidth="sm">
+          <Box my={1}>
+            <Box display="flex" justifyContent="center" mb={2}>
+              <img
+                src={logo}
+                alt="Logo"
+                style={{ maxWidth: "100%", height: "auto" }}
               />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Password"
-                variant="outlined"
-                type={showPassword ? 'text' : 'password'}
-                inputRef={passwordInput}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleTogglePasswordVisibility}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  )
-                }}
-              />
-            </Grid>
-            <Grid item xs={4} align="center">
-              <Button
-                fullWidth
-                variant="contained"
-                color="primary"
-                onClick={loginAction}
-              >
-                Log In
-              </Button>
-            </Grid>
-            <Grid item xs={12} align="center">
-              <Typography variant="body1">
-                Do you not have an account?
-              </Typography>
-              <Button
-                variant="outlined"
-                color="secondary"
-                onClick={newAction}
-              >
-                New Account
-              </Button>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Container>
+            </Box>
+            <Typography variant="h4" align="center" gutterBottom>
+              Welcome to
+            </Typography>
+            <Typography variant="h2" align="center" gutterBottom>
+              Fit Connect
+            </Typography>
+            <Typography variant="h4" align="center" gutterBottom>
+              Log in to your account
+            </Typography>
+            <Box component="form" noValidate autoComplete="off">
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Username"
+                    variant="outlined"
+                    inputRef={nameInput}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Password"
+                    variant="outlined"
+                    type={showPassword ? "text" : "password"}
+                    inputRef={passwordInput}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleTogglePasswordVisibility}
+                            edge="end"
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={4} align="center">
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    onClick={loginAction}
+                  >
+                    Log In
+                  </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                  <Typography variant="body1">
+                    Do you not have an account?
+                  </Typography>
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    onClick={newAction}
+                  >
+                    New Account
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
       )}
-    </div> 
-  )
+    </div>
+  );
 }
 
 export default HomePage;
